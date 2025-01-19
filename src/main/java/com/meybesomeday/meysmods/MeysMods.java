2b096924ccd74f0e63630f51b4a015328ad9d660
meybesomeday <meybesomeday@gmail.com>
2025-01-19 08:01:56 -0600

    replace examplemod with MeysMods

--- src/main/java/com/meybesomeday/meysmods/MeysMods.java
+++ src/main/java/com/meybesomeday/meysmods/MeysMods.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.meybesomeday.meysmods;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -30,8 +30,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(MeysMods.MODID)
+public class MeysMods
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -61,7 +61,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod()
+    public MeysMods()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
